712ee12dfb48dbe00ae1b4fbb1ea8b0c432380aa
Rajasri9 <61263400+Rajasri9@users.noreply.github.com>
2020-03-02 15:53:09 +0530

    Add files via upload

---
+++ dict with tuples,142.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 15:46:44 2020
+
+@author: rajas
+"""
+
+#Create a dictionary with following tuples (1,2), (2,3), (3,4).
+
+t=((1,2),(2,3), (3,4))
+dictionary=dict(t)
+print(dictionary)
